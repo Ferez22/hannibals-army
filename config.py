ee329f77707c2ac5e8dd19665403b603d11d81a7
Fares Aouani Cherif <fares.aouanicherif@mazars.de>
2026-03-03 20:40:51 +0000

    format code

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+import yaml
+
 # MAIN CONFIG FILE - BACKBONE OR YOUR APP
 
 APP_NAME = "Hannibal's Army"
 MASTER_MODEL = "llama3:8b"
 
-import yaml
 DIGITAL_TWIN = yaml.safe_load(open("digital-twin-config.yml"))
