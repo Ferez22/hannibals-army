ded195308ab49e557c53e89fc98dd7e552dbf732
Fares Aouani Cherif <fares.aouanicherif@mazars.de>
2026-03-19 11:28:13 +0100

    clean up tourist destination data and add documentation

--- utils/clean_trip_destinations_data.py
+++ utils/clean_trip_destinations_data.py
@@ -8,5 +8,11 @@ print("File exists:", os.path.exists("../data/tourist_destinations.csv"))
 data_csv = pd.read_csv("../data/tourist_destinations.csv")
 df = pd.DataFrame(data_csv)
 
-print(df)
+df_sorted = df.sort_values(by=["Country", "Destination Name"])  # use your actual column names
 
+# add a column named description that is empty
+df_sorted["Description"] = ""
+
+# save the sorted dataframe to a new csv file
+df_sorted.to_csv("../data/tourist_destinations_sorted.csv", index=False)
+print(df_sorted)
